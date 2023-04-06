6e0f44a3453d2d8865843352eae2b1ed32ce3362
Barthilas-WORK <snetivy.martin@seznam.cz>
2023-04-06 13:35:33 +0200

    XUnit introduction

---
+++ Tests/GuidGeneratorTests.cs
@@ -0,0 +1,72 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace xUnitTutorial.Tests
+{
+    [CollectionDefinition("Guid generator")]
+    public class GuidGeneratorDefinition: ICollectionFixture<GuidGenerator> { }
+
+    [Collection("Guid generator")]
+    public class GuidGeneratorTestsOne/* : IClassFixture<GuidGenerator>, IDisposable*/
+    {
+        private readonly GuidGenerator _guidGenerator;
+        private readonly ITestOutputHelper _output;
+
+        public GuidGeneratorTestsOne(ITestOutputHelper output, GuidGenerator guidGenerator)
+        {
+            _guidGenerator = guidGenerator;
+            _output = output;
+        }
+
+        [Fact]
+        public void GuidTestOne()
+        {
+            var guid = _guidGenerator.RandomGuid;
+            _output.WriteLine($"The guid was: {guid}");
+        }
+
+        [Fact]
+        public void GuidTestTwo()
+        {
+            var guid = _guidGenerator.RandomGuid;
+            _output.WriteLine($"The guid was: {guid}");
+        }
+
+        //public void Dispose()
+        //{
+        //    _output.WriteLine($"The class was disposed.");
+        //}
+    }
+
+    [Collection("Guid generator")]
+    public class GuidGeneratorTestsTwo/* : IClassFixture<GuidGenerator>, IDisposable*/
+    {
+        private readonly GuidGenerator _guidGenerator;
+        private readonly ITestOutputHelper _output;
+
+        public GuidGeneratorTestsTwo(ITestOutputHelper output, GuidGenerator guidGenerator)
+        {
+            _guidGenerator = guidGenerator;
+            _output = output;
+        }
+
+        [Fact]
+        public void GuidTestOne()
+        {
+            var guid = _guidGenerator.RandomGuid;
+            _output.WriteLine($"The guid was: {guid}");
+        }
+
+        [Fact]
+        public void GuidTestTwo()
+        {
+            var guid = _guidGenerator.RandomGuid;
+            _output.WriteLine($"The guid was: {guid}");
+        }
+
+        //public void Dispose()
+        //{
+        //    _output.WriteLine($"The class was disposed.");
+        //}
+    }
+}
